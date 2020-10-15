6673fd4d88fcc94f68bc2e2e8bf57564d955f8a6
Alejandro Hoyos <ahoyos@bluesmile-it.com>
2020-10-14 22:20:01 -0500

    Bet component and functions

--- src/App.js
+++ src/App.js
@@ -2,20 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import dado from './dado.svg';
 import './App.css';
+import Bet from './components/Bet';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={dado} className="App-logo" alt="logo" />
-        <br/>
-        <p>Valor de apuesta.</p>
-        <input className="redondeado confondo text-claro" placeholder="Valor de la apuesta"/>
-        <br/>
-        <button className="btn rounded"><span className="text-green">Lanzar Dado !!</span></button>
-        <p>La apuesta se: </p>
-        <label>"Perdio / Gano"</label>
-      </header>
+     <Bet number={5}></Bet>
     </div>
   );
 }
---
+++ src/components/Bet.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo from '../logo.svg';
+import dado from '../dado.svg';
+import { useEffect, useState } from "react"
+import PropTypes from 'prop-types';
+
+export default function Bet({ number }) {
+    const [result, setResult] = useState(0);
+    const [msg, setMsg] = useState('');
+
+    const play = () => {
+        setResult(Math.floor(Math.random() * 6));
+    }
+    const validate = () => {
+        if (result !== 0)
+            result == number ? setMsg('Apuesta ganada!😎') : setMsg('Apuesta perdida 😭')
+    }
+    
+    useEffect(validate, [result])
+
+    return (
+        <>
+            <header className="App-header">
+                <img src={dado} className="App-logo" alt="logo" />
+                <br />
+                <br />
+                <button onClick={play} className="btn rounded"><span className="text-green">Lanzar Dado !!</span></button>
+                <p>Resultado: </p>
+                <label>{result}</label>
+                <p>{msg}</p>
+
+            </header>
+        </>
+    )
+}
+Bet.propTypes = {
+    number: PropTypes.number.isRequired
+}
