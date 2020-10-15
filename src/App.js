610ca3d76932b6247158c57a6eee7cda4ae523a2
sebaspala02 <sebastianpala02@gmail.com>
2020-10-14 21:26:19 -0500

    layout of the ready partial 2do corte

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
+import dado from './dado.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={dado} className="App-logo" alt="logo" />
+        <br/>
+        <p>Valor de apuesta.</p>
+        <input className="redondeado confondo text-claro" placeholder="Valor de la apuesta"/>
+        <br/>
+        <button className="btn rounded"><span className="text-green">Lanzar Dado !!</span></button>
+        <p>La apuesta se: </p>
+        <label>"Perdio / Gano"</label>
       </header>
     </div>
   );
